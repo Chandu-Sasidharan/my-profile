198ff6203bdbaf079b506004895e9347ff257344
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-10-19 16:34:42 +0200

    minor content update and format

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 export default defineConfig({
-  server: {
-    port: 3000
-  },
-  plugins: [ViteMinifyPlugin({})],
+    server: {
+        port: 3000,
+    },
+    plugins: [ViteMinifyPlugin({})],
 });
